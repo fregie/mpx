bae3f70f04b18270e864d864f3445ed198b25d28
fregie <fregie@fregie.cn>
2022-05-31 23:17:35 +0800

    add TCPmultiDialer

--- dialer/tcp.go
+++ dialer/tcp.go
@@ -1,6 +1,10 @@
 package dialer
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"sync"
+)
 
 type TCPDialer struct {
 	RemoteAddr string
@@ -8,4 +12,21 @@ type TCPDialer struct {
 
 func (d *TCPDialer) Dial() (net.Conn, error) {
 	return net.Dial("tcp", d.RemoteAddr)
+}
+
+type TCPmultiDialer struct {
+	RemoteAddrs []string
+	offset      int
+	dialLock    sync.Mutex
+}
+
+func (d *TCPmultiDialer) Dial() (net.Conn, error) {
+	if len(d.RemoteAddrs) == 0 {
+		return nil, fmt.Errorf("no remote address")
+	}
+	d.dialLock.Lock()
+	defer d.dialLock.Unlock()
+	conn, err := net.Dial("tcp", d.RemoteAddrs[d.offset%len(d.RemoteAddrs)])
+	d.offset++
+	return conn, err
 }
